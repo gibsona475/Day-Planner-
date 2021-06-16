f81046dececdf31527981322262231052b0da0ac
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-06-15 20:27:41 -0700

    Updated Readme

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+console.log('h1');
+
+//$(function(){
+    //$('.timeContainer').css({color:'red',})
+  //$(".jumbotron").hide();
+//});
+
+//$(".timeContainer").on(click, function(){
+  //  $.fadetoggle(200);
+//});
+
